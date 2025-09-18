633c5a76cd50d7fb4f923c6f9b6f553902ba0353
sujanakhanal <sujanakhanal44@gmail.com>
2025-09-18 23:19:16 +0545

    header footer setting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,50 @@
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
   return (
-    <header className="bg-blue-500 text-white p-4">
-      <h1>Header Works!</h1>
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div mr-4>
+            <Link to="/">
+              <Logo width="70px"></Logo>
+            </Link>
+          </div>
+        </nav>
+      </Container>
     </header>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
+export { default as Container } from "./container/Container";
+export { default as Logo } from "./Logo";
+export { default as LogoutBtn } from "./Header/LououtBtn";
