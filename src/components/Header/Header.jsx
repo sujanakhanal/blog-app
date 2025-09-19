b96f55475a2a18b7012d0685a3e641bf850776af
sujanakhanal <sujanakhanal44@gmail.com>
2025-09-19 16:00:13 +0545

    components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,13 +38,29 @@ export default function Header() {
     <header className="py-3 shadow bg-gray-500">
       <Container>
         <nav className="flex">
-          <div mr-4>
+          <div className="mr-4">
             <Link to="/">
               <Logo width="70px"></Logo>
             </Link>
           </div>
-          <ul className="ml-4">
-            <li className="ml-6"></li>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-gray-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
           </ul>
         </nav>
       </Container>
