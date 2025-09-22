b2b89640a0272e45592c007e4c213278e85fcb00
sujanakhanal <sujanakhanal44@gmail.com>
2025-09-22 21:39:41 +0545

    hook

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import appwriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
+
+function PostCard({ $id, title, featuredImage }) {
+  return <Link></Link>;
+}
+
+export default PostCard;
